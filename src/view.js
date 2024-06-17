0a6d85dda5e1a5db627694b9da8b3aab8e27991d
M.Victoria Montero <mvictoria.monteroo@gmail.com>
2024-06-17 13:25:58 -0400

    pruebas en el view con el create element

--- src/view.js
+++ src/view.js
@@ -1,6 +1,23 @@
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
+
+  
   return 'example';
 };
 
+document.body.onload = addElement;
+
+function addElement() {
+  // crea un nuevo div
+  // y añade contenido
+  var newDiv = document.createElement("ul");
+  var newContent = document.createTextNode("Hola!¿Qué tal?");
+  newDiv.appendChild(newContent); //añade texto al div creado.
+
+  // añade el elemento creado y su contenido al DOM
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
+console.log();
