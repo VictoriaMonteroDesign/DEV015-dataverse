c75a6d4886d1a7756d8fb2498c13c22db31d4c8c
M.Victoria Montero <mvictoria.monteroo@gmail.com>
2024-06-19 15:44:13 -0400

    Se ha linkeado la data al view y al main, y ahora se muestra en el div del Html

--- src/view.js
+++ src/view.js
@@ -8,23 +8,31 @@ export const renderItems = (data) => {
     const nameElement = document.createElement('h3');
     nameElement.textContent = item.name;
 
-    const imageUrlElement = document.createElement('img');
-    imageUrlElement.textContent = item.imageUrl;
-
     const shortDescriptionElement = document.createElement('p');
     shortDescriptionElement.textContent = item.shortDescription;
+
+    const imageUrlElement = document.createElement('img');
+    imageUrlElement.setAttribute("src", item.imageUrl);
 
     const descriptionElement = document.createElement('p');
     descriptionElement.textContent = item.description;
 
     const categoryElement = document.createElement('p');
-    categoryElement.textContent = item.category;
-    
+    categoryElement.textContent = `Categoria: ${item.facts.category}`;
+
+    const yearOfCreationElement = document.createElement('p');
+    yearOfCreationElement.textContent = `Año de Creacion: ${item.facts.yearOfCreation}`;
+
+    const rankingElement = document.createElement('p');
+    rankingElement.textContent = `Calificacion: ${item.facts.ranking}`;
+
     li.appendChild(nameElement);
-    li.appendChild(imageUrlElement);
     li.appendChild(shortDescriptionElement);
+    li.appendChild(imageUrlElement);
     li.appendChild(descriptionElement);
     li.appendChild(categoryElement);
+    li.appendChild(yearOfCreationElement);
+    li.appendChild(rankingElement);
 
 
   ul.appendChild(li);
