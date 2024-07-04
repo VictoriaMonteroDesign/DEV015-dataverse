62ec69a50009b4a5ae90c257f47e00dc3c40cc43
M.Victoria Montero <mvictoria.monteroo@gmail.com>
2024-07-04 10:02:24 -0400

    se arreglan errores en los test de html

--- src/view.js
+++ src/view.js
@@ -4,6 +4,7 @@ export const renderItems = (data) => {
   const ul = document.createElement("ul");
   data.forEach(item => {
     const li = document.createElement('li');
+    // li.setAttribute("itemtype",)
 
     const nameElement = document.createElement('h3');
     nameElement.textContent = item.name;
@@ -14,8 +15,8 @@ export const renderItems = (data) => {
     const imageUrlElement = document.createElement('img');
     imageUrlElement.setAttribute("src", item.imageUrl);
 
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = item.description;
+    /*const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = item.description;*/
 
     const categoryElement = document.createElement('p');
     categoryElement.innerHTML = `<strong class="bold-text">Categoría:</strong> ${item.facts.category}`;
@@ -29,7 +30,7 @@ export const renderItems = (data) => {
     li.appendChild(nameElement);
     li.appendChild(shortDescriptionElement);
     li.appendChild(imageUrlElement);
-    li.appendChild(descriptionElement);
+    //li.appendChild(descriptionElement);
     li.appendChild(categoryElement);
     li.appendChild(yearOfCreationElement);
     li.appendChild(rankingElement);
